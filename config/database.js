167ee4fba6e251000e5cdfef57971190b4ee9e00
sonu kumar <124373321+sonukumar6343@users.noreply.github.com>
2024-11-27 17:08:39 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,16 +1,20 @@
-const {Sequelize}= require('sequelize');
+const { Sequelize } = require('sequelize');
 
-const sequelize =new Sequelize('assignment','root','hustler6343',{
-    host:'localhost',
-    dialect:'mysql',
-})
+const sequelize = new Sequelize('defaultdb', 'avnadmin', 'AVNS_3kHaXgyvtomuCPxjblN', {
+  host: 'mysql-2792a6b2-hustler6343.e.aivencloud.com',
+  dialect: 'mysql',
+  port: 11357,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,  
+  },
+});
 
-exports.dbconnection=async()=>{
-    try {
-        await sequelize.authenticate();
-        console.log("Connected to Mysql database using sequelize ")
-    } catch (error) {
-        console.log("unable to connect to Mysql database",error);
-        
-    }
+exports.dbconnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connected to MySQL database using Sequelize");
+  } catch (error) {
+    console.log("Unable to connect to MySQL database:", error);
+  }
 };
